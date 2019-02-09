98d96cd5fec96a0d9ab873e07e177b540eb13454
badgamerbad <badgamerbad@gmail.com>
2019-02-09 12:07:11 -0500

    polishing products

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,33 +8,47 @@ import './products.scss'
 import { graphql } from "gatsby"
 import get from "lodash/get"
 
-const Products = (props) => {
-  const products = get(props, "data.allMarkdownRemark.edges").map(
-    edge => edge.node.frontmatter
-  ); 
-  // note that if using class component this data will exist on this.props
-  // map is to return data only from front matter
-  return (
-    <Layout>
-      <SEO title="Produts" />
-      <div className="products">
-        <div className="filters">
-          <label>Name:</label>
-          <input type="text"/>
-        </div>
-        <div className="content">
-          <div className="count"></div>
-          <div className="listing">
-            {
-              products.map(product => (
-                <li key={product.name}>{product.name}</li>
-              ))
-            }
+class Products extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+  componentDidMount(){
+    this.setState({
+      products: get(this.props, "data.allMarkdownRemark.edges").map(
+        edge => edge.node.frontmatter
+      )
+    })
+  }
+  render() {
+    return (
+      <Layout>
+        <SEO title="Produts" />
+        <div className="products">
+          <div className="filters">
+            <label>Name:</label>
+            <input type="text"/>
+            <button onClick={ () => this.triggerSearch() }>Search</button>
+          </div>
+          <div className="content">
+            <div className="count"></div>
+            <div className="listing">
+              {
+                this.state.products.map(product => (
+                  <li key={product.name}>{product.name}</li>
+                ))
+              }
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
+  triggerSearch() {
+    console.log("search")
+  }
 }
 
 export default Products

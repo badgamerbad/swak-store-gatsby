db4c3251a6caa03517437e1db6dce5aa890020a2
badgamerbad <badgamerbad@gmail.com>
2019-02-14 23:42:47 +0530

    upsFilter.js: fixed radio selection

--- src/components/filters/upsFilters/upsFilter.js
+++ src/components/filters/upsFilters/upsFilter.js
@@ -1,42 +1,46 @@
-import React from "react"
-
+import React, { Component } from "react"
 import './upsFilter.scss'
 
-const UpsFilter = ({filter}) => {
-    return (
-        <div className="ups-filter">
-            <label>{filter.node.frontmatter.name}</label>
-            <ul>
-                <li>
-                    <ul>
-                        <li>All</li>
-                        <li><input type="radio" value={-1} checked={isChecked.bind(this) === -1} onChange={onChanged.bind(this)} name={filter.node.frontmatter.name}/></li>
-                    </ul>
-                </li>
-                {
-                    filter.node.frontmatter.value.map( 
-                        (item, index) => 
-                        <li key={index}> 
-                            <ul>
-                                <li>{item}</li>
-                                <li><input type="radio" value={index} checked={isChecked.bind(this) === index} onChange={onChanged.bind(this)} name={filter.node.frontmatter.name}/></li>
-                            </ul>
-                        </li>
-                    )
-                }
-            </ul>
-        </div>
-    )
-}
 
-const onChanged = (e) => {
-    console.log('on changed')
-    console.log(e)
-    return parseInt(e.currentTarget.value)
-}
-const isChecked = (e) => {
-    console.log('Is checked')
-    console.log(e)
+class UpsFilter extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            filter: props.filter.node.frontmatter,
+            index: -1
+        }
+    }
+    render() {
+        return (
+            <div className="ups-filter">
+                <label>{this.state.filter.name}</label>
+                <ul>
+                    <li>
+                        <ul>
+                            <li>All</li>
+                            <li><input type="radio" value={-1} checked={this.state.index === -1} onChange={this.onChanged.bind(this, -1)} name={this.state.filter.name}/></li>
+                        </ul>
+                    </li>
+                    {
+                        this.state.filter.value.map( 
+                            (item, index) => 
+                            <li key={index}> 
+                                <ul>
+                                    <li>{item}</li>
+                                    <li><input type="radio" value={index} checked={this.state.index === index} onChange={this.onChanged.bind(this, index)} name={this.state.filter.name}/></li>
+                                </ul>
+                            </li>
+                        )
+                    }
+                </ul>
+            </div>
+        )
+    }
+    onChanged(e, i) {
+        this.setState({
+            index: parseInt(e)
+        })
+    }
 }
 
 export default UpsFilter
